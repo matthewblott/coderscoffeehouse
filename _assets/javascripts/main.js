9af89c80d161bdd1a8a7087f379cf012aecae96e
Matthew Blott <Matt@Matthews-MacBook-Pro.local>
2015-10-10 20:10:08 +0100

    automated update

---
+++ _assets/javascripts/main.js
@@ -0,0 +1,15 @@
+//= require vendor/jquery-2.1.4
+//= require vendor/bootstrap
+//= require vendor/lunr
+//= require vendor/mustache
+//= require vendor/date.format
+//= require vendor/URI
+//= require vendor/jquery.lunr.search
+
+$(function() {
+    $('#search-query').lunrSearch({
+        results: '#search-results',
+        entries: '.entries',
+        template: '#search-results-template'
+    });
+});
--- _posts/2015-10-07-hello-world.md
+++
@@ -1,8 +0,0 @@
----
-layout: post
-title:  "Hello World!"
-date:   2015-10-07 21:24:02
-categories: jekyll
----
-
-This is a simple Hello World demo
