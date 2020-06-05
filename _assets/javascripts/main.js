29b61d34f710d9348ad2d1f40402fb50e3bad405
Matthew Blott <matthew.blott@gmail>
2020-06-05 18:50:49 +0100

    saved changes

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -1,15 +1,1 @@
-//= require libs/jquery-2.2.0
-//= require libs/tether-1.3.2
 //= require libs/bootstrap
-//= require libs/lunr-0.4.5
-//= require libs/date.format-1.2.3
-//= require libs/URI-1.12.1
-//= require libs/jquery.lunr.search-0.0.1
-
-$(function () {
-  $('#search-query').lunrSearch({
-    results: '#search-results',
-    entries: '.entries',
-    template: '#search-results-template'
-  });
-});
